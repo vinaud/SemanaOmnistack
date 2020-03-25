d0690aaab6c06a6052ef6a1598f7bc58b7470ec5
João Vinaud <leyenasd@gmail.com>
2020-03-25 10:01:53 -0300

    Adicionado arquivo de Header

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,28 +1,9 @@
 import React from 'react';
-
+import Header from './Header';
 
 function App() {
     return ( <
-        div className = "App" >
-        <
-        header className = "App-header" >
-        <
-        img src = { logo }
-        className = "App-logo"
-        alt = "logo" / >
-        <
-        p >
-        <
-        code > Hello World < /code> <
-        /p> <
-        a className = "App-link"
-        href = "https://reactjs.org"
-        target = "_blank"
-        rel = "noopener noreferrer" >
-        Learn React <
-        /a> <
-        /header> <
-        /div>
+        Header / >
     );
 }
 
