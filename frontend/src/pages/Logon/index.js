226cdbf325c9f31a935d4a9006af58cd92735aa6
João Vinaud <leyenasd@gmail.com>
2020-03-25 19:56:53 -0300

    Listagem dos casos da ong e logout implementados

--- frontend/src/pages/Logon/index.js
+++ frontend/src/pages/Logon/index.js
@@ -1,20 +1,42 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './styles.css';
 import {FiLogIn} from 'react-icons/fi';
-import {Link} from 'react-router-dom';
+import {Link, useHistory} from 'react-router-dom';
 
 import heroesImg from '../../assets/heroes.png';
 import logoImg from '../../assets/logo.svg';
 
-export default function logon() {
+import api from '../../Services/api';
+
+export default function Logon() {
+    const [id, setId] = useState('');
+    const history = useHistory();
+
+    async function handleLogin(e)
+    {
+        e.preventDefault();
+
+        try{
+            const response = await api.post('sessions', {id});
+            localStorage.setItem('ongId', id);
+            localStorage.setItem('ongName', response.data.name);
+
+            history.push('profile');
+        }
+
+        catch(err)
+        {
+            alert('Falha no login, tente novamente');
+        }
+    }
 
     return (
         <div className="logon-container">
             <section className="form">
                 <img src={logoImg} alt="Be the Heroes" />
-                <form>
+                <form onSubmit = {handleLogin}>
                     <h1>Faça seu logon:</h1>
-                    <input placeholder="Insira Sua ID"/>
+                    <input placeholder="Insira Sua ID" value = {id} onChange = {e => setId(e.target.value)} />
                     <button className="button" type="submit">Entrar</button>
                     <Link className ="back-link" to="/register"> <FiLogIn size={16} color = "#e02041"/> Não tenho cadastro</Link>
                 </form>
