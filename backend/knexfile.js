9a05dcd9f37fae25e3df81a96a58a8b95b3c9187
João Vinaud <leyenasd@gmail.com>
2020-03-27 15:29:20 -0300

    Configuração dos testes de integração

--- backend/knexfile.js
+++ backend/knexfile.js
@@ -13,6 +13,17 @@ module.exports = {
         },
     },
 
+    test: {
+        client: 'sqlite3',
+        connection: {
+            filename: './src/database/test.sqlite'
+        },
+
+        migrations: {
+            directory: './src/database/migrations'
+        },
+    },
+
 
 
     staging: {
